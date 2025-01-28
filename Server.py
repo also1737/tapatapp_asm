1a179ee7f7921819ea38e6740dbd7ac7615f8aad
also1737 <albaricoque.sotot@gmail.com>
2025-01-28 14:44:37 +0100

    devolver datos en formato json

--- Server.py
+++ Server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 
+# clase de usuari
 class User:
     def __init__(self, id, username, password, email=""):
         self.id = id
@@ -10,6 +11,8 @@ class User:
     def __str__(self):
         return "Id: " + str(self.id) + " Username: " + self.username
 
+
+# llista dels usuaris admesos
 listUsers = [
     User(1, "pepe", "1234", "pepe@gmail.com"),
     User(2, "pepito", "2411", "pepito@gmail.com"),
@@ -17,6 +20,8 @@ listUsers = [
     User(4, "admin", "rootroot")
 ]
 
+# clase que farem servir per accedir als usuaris
+# DAO = Data Access Object
 class DAOUsers:
     def __init__(self):
         self.users = listUsers
@@ -24,12 +29,17 @@ class DAOUsers:
     def getUserByUsername(self, username):
         for u in self.users:
             if u.username == username:
-                return u
+                return u.__dict__
         return None
 
 daoUser = DAOUsers()
 
 app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def root():
+    return "hola hola"
+
 
 @app.route('/tapatapp/getuser', methods=['GET'])
 def getUser():
@@ -37,13 +47,16 @@ def getUser():
     email = str( request.args.get('email') )
     return "hola hola nom: " + n + ", email: " + email
 
+
+
 @app.route('/prototip/getuser/<string:user>', methods=['GET'])
-def provaGetUser(user):
+def getUserPrototip(user):
     u = daoUser.getUserByUsername(user)
     if (u):
-        return "Usuari trobat: " + u.__str__()
+        return jsonify(u)
     else:
-        return "Usuari no trobat"
+        return jsonify({"error":"usuari no trobat"})
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port="10050")
